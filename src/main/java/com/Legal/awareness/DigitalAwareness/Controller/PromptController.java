a71f09c7419900fcc81de00e3c4fd30bd17935d2
DivyanshNM <www.divyanshmishra2005@gmail.com>
2026-06-03 10:54:14 +0530

    Beautiful response

--- src/main/java/com/Legal/awareness/DigitalAwareness/Controller/PromptController.java
+++ src/main/java/com/Legal/awareness/DigitalAwareness/Controller/PromptController.java
@@ -3,7 +3,6 @@ package com.Legal.awareness.DigitalAwareness.Controller;
 import com.Legal.awareness.DigitalAwareness.entity.prompt;
 import com.Legal.awareness.DigitalAwareness.entity.reply;
 import com.Legal.awareness.DigitalAwareness.service.promptService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
